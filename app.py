bc97b83d798a8c03556a032fc2466b57039ac2f3
SulimanMahjna <elriced700@gmail.com>
2025-10-29 11:38:43 +0300

    final version

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
-from flask import Flask , render_template , request
+from flask import Flask , render_template , request , url_for , redirect
 app = Flask(__name__)
+
 @app.route("/")
 def index():
-     return render_template('index.html')
+     data = request.args.get("data")
+     return render_template('index.html' , data=data)
 
-@app.route('/generate', methods=['GET', 'POST'])
-def genarete():
-    message = "Initial Text"
-    if request.method == 'POST':
-        if 'change_text_btn' in request.form:
-            message = "Text Changed by Button!"
-    return render_template('index.html', message=message)
+@app.route('/generate', methods=["POST"])
+def generate():
+    if request.method == "POST":
+         first_name = request.form["first_name"]
+         last_name = request.form["last_name"]
+         job_titel = request.form["job_titel"]
+
+         response = f"{first_name} , {last_name} , {job_titel}"
+    return redirect( url_for("index" , data=response))
+
 
 if __name__ == "__main__": 
      app.run(debug=True, port=3003)
