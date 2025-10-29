c7eaf83ca0a14f2f32431ffb5b406446dd1e64a6
SulimanMahjna <elriced700@gmail.com>
2025-10-29 10:34:23 +0300

    add req.txt

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , request
 app = Flask(__name__)
 @app.route("/")
 def index():
      return render_template('index.html')
+
+@app.route('/generate', methods=['GET', 'POST'])
+def genarete():
+    message = "Initial Text"
+    if request.method == 'POST':
+        if 'change_text_btn' in request.form:
+            message = "Text Changed by Button!"
+    return render_template('index.html', message=message)
+
 if __name__ == "__main__": 
      app.run(debug=True, port=3003)
